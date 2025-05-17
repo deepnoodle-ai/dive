6bdc3b66bbba6a541a33f90cdc5368fed83e1ba7
Sergio Rubio <rubiojr@frameos.org>
2025-05-17 19:57:58 +0200

    Add Kagi search tool and example
    The example uses Kagi and an Azure OpenAI (same API, different endpoint)
    to research the history of computing.

--- examples/programs/search_kagi/main.go
+++ examples/programs/search_kagi/main.go
@@ -9,16 +9,23 @@ import (
 	"github.com/diveagents/dive"
 	"github.com/diveagents/dive/agent"
 	"github.com/diveagents/dive/llm"
-	"github.com/diveagents/dive/llm/providers/anthropic"
+	"github.com/diveagents/dive/llm/providers/openai"
 	"github.com/diveagents/dive/slogger"
 	"github.com/diveagents/dive/toolkit"
 	"github.com/diveagents/dive/toolkit/kagi"
 )
 
-// You'll need to have these set:
+// Use Kagi and Azure OpenAI to research the history of computing.
+//
+// Note Kagi search API is currently in private beta, see https://help.kagi.com/kagi/api/search.html to request an invite.
+//
+// You'll need to have these environment variables set:
 // - KAGI_API_KEY
-// - OPEN
-// https://developers.google.com/custom-search/v1/introduction
+// - OPENAI_ENDPOINT
+//   - Something like https://YOUR_RESOURCE_NAME.openai.azure.com/openai/deployments/YOUR_DEPLOYMENT_NAME/chat/completions?api-version=2024-06-01
+//   - See https://learn.microsoft.com/en-us/azure/ai-services/openai/reference
+//
+// - OPENAI_API_KEY
 func main() {
 
 	var logLevel string
@@ -30,17 +37,20 @@ func main() {
 
 	ctx := context.Background()
 
-	googleClient, err := kagi.New()
+	kagi, err := kagi.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	model := openai.New(
+		openai.WithEndpoint(os.Getenv("OPENAI_ENDPOINT")),
+	)
 	researcher, err := agent.New(agent.Options{
 		Name:   "Research Assistant",
-		Goal:   "Use Google to research assigned topics",
-		Model:  anthropic.New(),
+		Goal:   "Use Kagi to research assigned topics",
+		Model:  model,
 		Logger: slogger.New(slogger.LevelFromString(logLevel)),
-		Tools:  []llm.Tool{toolkit.NewSearchTool(googleClient)},
+		Tools:  []llm.Tool{toolkit.NewSearchTool(kagi)},
 	})
 	if err != nil {
 		log.Fatal(err)
